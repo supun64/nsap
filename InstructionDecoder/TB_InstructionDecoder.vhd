864b2ab37c1c3a1287c3084c2395402f58caa902
supun64 <l.w.supunmadusanka64@gmail.com>
2021-03-30 10:14:53 +0530

    Created TB

---
+++ InstructionDecoder/TB_InstructionDecoder.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity TB_InstructionDecoder is
+end TB_InstructionDecoder;
+
+architecture Behavioral of TB_InstructionDecoder is
+    component InstructionDecoder
+    port()
+
+begin
+
+
+end Behavioral;
